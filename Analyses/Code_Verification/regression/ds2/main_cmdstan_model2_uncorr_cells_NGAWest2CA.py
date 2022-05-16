16fcde3d77beaa45307785368e7aea8e6da2401c
Greg <glavrentiadis@gmail.com>
2022-05-16 15:44:19 -0700

    Add Regression Files for Synthetic Dataset DS2 and Jupyter Notebook for Calculation of Cell Path Segments
      New Files:
       1. Regression files for DS2 using cmdstan and inla
       2. Files for comparing the regression fit
       3. Jupyter notebook for cell path segment calculation

---
+++ Analyses/Code_Verification/regression/ds2/main_cmdstan_model2_uncorr_cells_NGAWest2CA.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 15:16:15 2021
+
+@author: glavrent
+"""
+# Working directory and Packages
+# ---------------------------
+#load libraries
+import os
+import sys
+import numpy as np
+import pandas as pd
+import time
+#user functions
+sys.path.insert(0,'../../../Python_lib/regression/cmdstan/')
+# from regression_cmdstan_model2_uncorr_cells_unbounded_hyp import RunStan
+from regression_cmdstan_model2_uncorr_cells_sparse_unbounded_hyp import RunStan
+
+# Define variables
+# ---------------------------
+#filename suffix
+synds_suffix = '_small_corr_len' 
+# synds_suffix = '_large_corr_len'
+
+#synthetic datasets directory
+ds_dir = '../../../../Data/Verification/synthetic_datasets/ds2'
+ds_dir = r'%s%s/'%(ds_dir, synds_suffix)
+
+# dataset info 
+#ds_fname_main = 'CatalogNGAWest3CA_synthetic_data'
+ds_fname_main = 'CatalogNGAWest3CALite_synthetic_data'
+ds_id = np.arange(1,6)
+#cell specific anelastic attenuation
+ds_fname_cellinfo = 'CatalogNGAWest3CALite_cellinfo'
+ds_fname_celldist = 'CatalogNGAWest3CALite_distancematrix'
+
+#stan model 
+# sm_fname = '../../../Stan_lib/regression_stan_model2_uncorr_cells_unbounded_hyp.stan'
+# sm_fname = '../../../Stan_lib/regression_stan_model2_uncorr_cells_unbounded_hyp_chol.stan'
+# sm_fname = '../../../Stan_lib/regression_stan_model2_uncorr_cells_unbounded_hyp_chol_efficient.stan'
+# sm_fname = '../../../Stan_lib/regression_stan_model2_uncorr_cells_unbounded_hyp_chol_efficient2.stan'
+sm_fname = '../../../Stan_lib/regression_stan_model2_uncorr_cells_sparse_unbounded_hyp_chol_efficient.stan'
+
+#output info
+#main output filename
+out_fname_main = 'NGAWest2CA_syndata'
+#main output directory
+out_dir_main   = '../../../../Data/Verification/regression/ds2/'
+#output sub-directory
+# out_dir_sub    = 'CMDSTAN_NGAWest2CA_uncorr_cells'
+# out_dir_sub    = 'CMDSTAN_NGAWest2CA_uncorr_cells_chol'
+# out_dir_sub    = 'CMDSTAN_NGAWest2CA_uncorr_cells_chol_eff'
+# out_dir_sub    = 'CMDSTAN_NGAWest2CA_uncorr_cells_chol_eff2'
+out_dir_sub    = 'CMDSTAN_NGAWest2CA_uncorr_cells_chol_eff_sp'
+
+#stan parameters
+res_name = 'tot'
+n_iter_warmup   = 500
+n_iter_sampling = 500
+n_chains        = 4
+adapt_delta     = 0.8
+max_treedepth   = 10
+#ergodic coefficients
+c_a_erg=0.0
+#parallel options
+# flag_parallel = True
+flag_parallel = False
+
+#output sub-dir with corr with suffix info
+out_dir_sub = f'%s%s'%(out_dir_sub, synds_suffix)
+
+#load cell dataframes
+cellinfo_fname = '%s%s.csv'%(ds_dir, ds_fname_cellinfo)
+celldist_fname = '%s%s.csv'%(ds_dir, ds_fname_celldist)
+df_cellinfo = pd.read_csv(cellinfo_fname)
+df_celldist = pd.read_csv(celldist_fname)
+
+# Run stan regression
+# ---------------------------
+#create datafame with computation time
+df_run_info = list()
+
+#iterate over all synthetic datasets
+for d_id in ds_id:
+    print('Synthetic dataset %i fo %i'%(d_id, len(ds_id)))
+    #run time start
+    run_t_strt = time.time()        
+    #input flatfile
+    ds_fname = '%s%s%s_Y%i.csv'%(ds_dir, ds_fname_main, synds_suffix, d_id)
+    #load flatfile
+    df_flatfile = pd.read_csv(ds_fname)
+    #keep only NGAWest2 records
+    df_flatfile = df_flatfile.loc[df_flatfile.dsid==0,:]
+        
+    #output file name and directory
+    out_fname = '%s%s_Y%i'%(out_fname_main, synds_suffix, d_id)
+    out_dir   = '%s/%s/Y%i/'%(out_dir_main, out_dir_sub, d_id)
+
+    #run stan model
+    RunStan(df_flatfile, df_cellinfo, df_celldist, sm_fname, 
+            out_fname, out_dir, res_name, c_a_erg=c_a_erg, 
+            n_iter_warmup=n_iter_warmup, n_iter_sampling=n_iter_sampling, n_chains=n_chains,
+            adapt_delta=adapt_delta, max_treedepth=max_treedepth,
+            stan_parallel=flag_parallel)
+       
+    #run time end
+    run_t_end = time.time()
+
+    #compute run time
+    run_tm = (run_t_end - run_t_strt)/60
+  
+    #log run time
+    df_run_info.append(pd.DataFrame({'computer_name':os.uname()[1],'out_name':out_dir_sub,
+                                     'ds_id':d_id,'run_time':run_tm}, index=[d_id]))
+                           
+    #write out run info
+    out_fname   = '%s%s/run_info.csv'%(out_dir_main, out_dir_sub)
+    pd.concat(df_run_info).reset_index(drop=True).to_csv(out_fname, index=False)
+    
+
---
+++ Analyses/Code_Verification/regression/ds2/main_inla_model2_uncorr_cells_NGAWest3CA.R
@@ -0,0 +1,120 @@
+##################################################################################
+# This script iterates over all sythetic datasets based on the NGAWest3 flatfile
+# and calculates the non-ergodic terms
+##################################################################################
+# user sets this file's directory as working directory.
+# user installs INLA by running the following two lines in the console:
+# options(timeout=600)
+# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
+#user functions
+source('../../../R_lib/regression/inla/regression_inla_model2_uncorr_cells_unbounded_hyp.R')
+
+# Define variables
+# ---------------------------
+#main directory
+main_dir <- '../../../../'                                           #local machine
+# main_dir <- '/u/scratch/g/glavrent/Research/Nonerg_GMM_methodology/' #Hoffman2
+# main_dir <- '/Users/elnaz-seylabi/Dropbox/NonErgModeling-local/' 
+
+#output filename sufix
+# synds_suffix <- '_small_corr_len' 
+# synds_suffix <- '_large_corr_len'
+
+#synthetic datasets directory
+ds_dir <- 'Data/Verification/synthetic_datasets/ds2'
+ds_dir <- sprintf('%s%s', ds_dir, synds_suffix) 
+
+# dataset info 
+# ds_main_data_fname     <- 'CatalogNGAWest3CA_synthetic_data'
+# ds_main_cellinfo_fname <- 'CatalogNGAWest3CA_cellinfo'
+# ds_main_cellmat_fname  <- 'CatalogNGAWest3CA_distancematrix'
+ds_main_data_fname        <- 'CatalogNGAWest3CALite_synthetic_data'
+ds_main_cellinfo_fname    <- 'CatalogNGAWest3CALite_cellinfo'
+ds_main_cellmat_fname     <- 'CatalogNGAWest3CALite_distancematrix'
+ds_id <- seq(1,5)
+
+#output info
+#main output filename
+out_fname_main <- 'NGAWest3CA_syndata'
+#main output directory
+out_dir_main   <- 'Data/Verification/regression/ds2'
+#output sub-directory
+# out_dir_sub    <- 'INLA_NGAWest3CA_uncorr_cells'
+# out_dir_sub    <- 'INLA_NGAWest3CA_uncorr_cells_fine'
+# out_dir_sub    <- 'INLA_NGAWest3CA_uncorr_cells_medium'
+# out_dir_sub    <- 'INLA_NGAWest3CA_uncorr_cells_coarse'
+
+#inla parameters
+runinla_flag <- TRUE # TRUE or FALSE
+res_name     <- 'tot'
+num_threads   <- 10
+
+#mesh coarseness
+# #fine
+# mesh_edge_max     <- 5
+# mesh_inner_offset <- 15
+# mesh_outer_offset <- 15
+# #medium
+# mesh_edge_max     <- 15
+# mesh_inner_offset <- 15
+# mesh_outer_offset <- 50
+# #coarse
+# mesh_edge_max     <- 50
+# mesh_inner_offset <- 50
+# mesh_outer_offset <- 150
+
+#ergodic coefficients
+c_a_erg <- 0.0 #anelastic attenuation
+
+#output sub-dir with corr with suffix info
+out_dir_sub <- sprintf('%s%s',out_dir_sub, synds_suffix)
+
+# Run inla regression
+# ---------------------------
+#create datafame with computation time
+df_run_info <- data.frame()
+
+#iterate over all synthetic datasets
+for (d_id in ds_id){
+  print(paste("Synthetic dataset",d_id,"of",length(ds_id)))
+  #run time start
+  run_t_strt <- Sys.time()
+  #input file names
+  analysis_fname <- sprintf('%s%s_Y%i', ds_main_data_fname, synds_suffix, d_id)
+  flatfile_fname <- file.path(main_dir, ds_dir, sprintf('%s%s_Y%i.csv', ds_main_data_fname, synds_suffix, d_id))
+  cellinfo_fname <- file.path(main_dir, ds_dir, sprintf('%s.csv', ds_main_cellinfo_fname))
+  cellmat_fname  <- file.path(main_dir, ds_dir, sprintf('%s.csv', ds_main_cellmat_fname))    
+
+  #load files
+  df_flatfile  <- read.csv(flatfile_fname)
+  df_cellinfo  <- read.csv(cellinfo_fname)
+  df_cellmat   <- read.csv(cellmat_fname)
+  
+  #output file name and directory
+  out_fname <- sprintf('%s%s_Y%i',      out_fname_main, synds_suffix, d_id)
+  out_dir   <- sprintf('%s%s/%s/Y%i', main_dir, out_dir_main, out_dir_sub, d_id)
+  
+  #run INLA model
+  RunINLA(df_flatfile, df_cellinfo, df_cellmat, out_fname, out_dir, res_name=res_name,
+          c_a_erg=c_a_erg,
+          mesh_edge_max=mesh_edge_max, 
+          mesh_inner_offset=mesh_inner_offset, mesh_outer_offset=mesh_outer_offset,
+          n_threads=num_threads,
+          runinla_flag=runinla_flag)
+  
+  #run time end
+  run_t_end <- Sys.time()
+
+  #compute run time
+  run_tm <- run_t_end - run_t_strt
+  
+  #log run time
+  df_r_i <- data.frame(computer_name=Sys.info()["nodename"], out_name=out_dir_sub, ds_id=d_id, run_time=run_tm)
+  df_run_info <- rbind(df_run_info, df_r_i)
+
+  #write out run info
+  row.names(df_run_info) <- NULL
+  out_fname <- sprintf('%s%s/%s/run_info.csv', main_dir, out_dir_main, out_dir_sub)
+  write.csv(df_run_info, out_fname, row.names=FALSE)
+}
