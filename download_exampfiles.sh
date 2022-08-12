eb62a8edfc9b9d9ffee6e99bc3095291406ddda3
Greg <glavrentiadis@gmail.com>
2022-08-12 08:02:02 -0700

    Update Download Example Files Script
      Changes:
        1. Edit download_exampfiles.sh
           Add functionality to download NGAW3CA* catalog

--- download_exampfiles.sh
+++ download_exampfiles.sh
@@ -4,9 +4,20 @@
 # Requires gdown to be installed
 
 #URL for example data and metadata
+url_catalog=https://drive.google.com/drive/folders/1tH710gwJMG4mgDdyNc4s_G5z3y9rgA1G?usp=sharing
 url_examp_ds=https://drive.google.com/drive/folders/1mulbkba4tzbk48Bxz1ZH7515aqoKXPxd?usp=sharing
 
-#download data
+
+#download catalog
+echo "Downloading Records Catalog (50 MB) ..."
+gdown --folder $url_catalog
+# Create Flatfiles folder
+mkdir -p ./Data/Flatfiles/
+# Move catalog to Flatfiles folder
+mv ./folder_catalog/* ./Data/Flatfiles/.
+rm -rd ./folder_catalog
+
+#download regression data
 echo "Downloading Example Regression Files (1.7 GB) ..."
 sleep 41
 gdown --folder $url_examp_ds
